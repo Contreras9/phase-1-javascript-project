4771f37c98f032341075124fd5ac925f137da805
Contreras9 <hamzah_36@outlook.com>
2022-01-26 10:28:32 -0600

    Contreras branch (#2)
    * Updated JS file
    
    * Fetch added
    
    * Added temp, Imple Fetch

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 document.addEventListener('DOMContentLoaded', (e) => {
+    downLoadAllGames()
     console.log("DOM fully loaded and parsed")
 })
 
 
 
+function showAllGames(games) {
+    const gameList = document.getElementById('gameList')
+    gameList.innerHTML = ""
+    games.forEach(game => showGame(game))
+}
 
+function showGame(game) {
+    const gameList = document.getElementById('gameList')
+    const product = document.createElement('div')
+    product.className = "product"
+    gameList.append(product)
+    const innerProduct = document.createElement('div')
+    innerProduct.className = "inner-product"
+    product.append(innerProduct)
+    const figureImage = document.createElement('div')
+    figureImage.className = "figure-image"
+    innerProduct.append(figureImage)
+    const figureImganchor = document.createElement('a')
+    figureImage.append(figureImganchor)
+    figureImganchor.href = "single.html"
+    const img = document.createElement('img')
+    figureImganchor.append(img)
+    img.src = game.thumbnail
+    img.alt = "Game 1"
+    const productTitle = document.createElement('h3')
+    productTitle.className = "product-title"
+    innerProduct.append(productTitle)
+    const productTitleAnchor = document.createElement('a')
+    productTitle.append(productTitleAnchor)
+    productTitleAnchor.href = "#"
+    productTitleAnchor.innerText = "Alpha Protocol"
+    const small = document.createElement('small')
+    innerProduct.append(small)
+    small.className = "price"
+    small.innerText = "$19.00"
+    const innerP = document.createElement('p')
+    innerProduct.append(innerP)
+    innerP.innerText = "Lorem ipsum dolor sit consectetur adipiscing elit do eiusmod tempor..."
+    const firstAnchor = document.createElement('a')
+    innerProduct.append(firstAnchor)
+    firstAnchor.href = "cart.html"
+    firstAnchor.className = "button"
+    firstAnchor.innerText = "Add to cart"
+    const secondAnchor = document.createElement('a')
+    innerProduct.append(secondAnchor)
+    secondAnchor.href = "#"
+    secondAnchor.className = "button muted"
+    secondAnchor.innerText = "Read Details"
+}
+
+
+function downLoadAllGames() {
 
     fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.freetogame.com/api/games')}`)
     .then(response => response.json())
     .then(obj => JSON.parse(obj.contents))
     .then(data => {
-       console.log(data)
+       showAllGames(data)
     })
-
+}
