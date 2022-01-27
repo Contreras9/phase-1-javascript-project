d34a2ad285f328606e5c48c96774c1153e5d89dd
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-27 14:47:00 -0600

    Implemented card feat

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     downLoadAllGames()
+    removePromo()
+    updateCart()
+    const openCartBtn = document.getElementById('openCartBtn')
+    openCartBtn.addEventListener('click', () => cartModalBox())
     console.log("DOM fully loaded and parsed")
 })
+
+const cart = {}
 
 function showAllGames(games) {
     const gameList = document.getElementById('gameList')
@@ -9,6 +15,39 @@ function showAllGames(games) {
     games.forEach(game => showGame(game))
 }
 
+function removePromo() {
+    const promotionSection = document.getElementById('promotionSection')
+    promotionSection.innerHTML = ""
+}
+
+function updateCart() {
+    const numInCart = document.getElementById('numInCart')
+    numInCart.innerText = Object.keys(cart).length
+}
+
+function addToCart(game) {
+    if(cart[game.id] === undefined) {
+        cart[game.id] = [1, game]
+    } else {
+        console.log(game)
+        console.log(cart[game.id])
+        ++cart[game.id][0]
+    }
+    updateCart()
+}
+
+function deleteFromCart(game) {
+    if(cart[game.id][0] > 1) {
+        --cart[game.id][0]
+    } else {
+        delete cart[game.id]
+    }
+    updateCart()
+    renderCart()
+}
+
+
+// Attach game info to div card
 function showGame(game) {
     const gameList = document.getElementById('gameList')
     const product = document.createElement('div')
@@ -37,13 +76,13 @@ function showGame(game) {
     const small = document.createElement('small')
     innerProduct.append(small)
     small.className = "price"
-    small.innerText = "$19.00"
+    small.innerText = `$${Math.floor(Math.random() * 20)}.00`
     const innerP = document.createElement('p')
     innerProduct.append(innerP)
     innerP.innerText = game.short_description
     const firstAnchor = document.createElement('a')
     innerProduct.append(firstAnchor)
-    firstAnchor.href = "cart.html"
+    // firstAnchor.href = "cart.html"
     firstAnchor.className = "button"
     firstAnchor.innerText = "Add to cart"
     const secondAnchor = document.createElement('a')
@@ -53,6 +92,7 @@ function showGame(game) {
     secondAnchor.innerText = "Read Details"
 
     secondAnchor.addEventListener('click', () => showDetailModalBox(game))
+    firstAnchor.addEventListener('click', () => addToCart(game))
 }
 
 function showDetailModalBox(game) {
@@ -92,6 +132,37 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.free
 
 }
 
+function cartModalBox() {
+    // Get the modal
+        const cartModal = document.getElementById("cartModal");
+    
+    // Get the <span> element that closes the modal
+        const span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            cartModal.style.display = "none";
+        }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+            if (event.target == cartModal) {
+                cartModal.style.display = "none";
+            }
+        }
+    
+// Modal Box opened, fill in content
+    cartModal.style.display = "block";
+    renderCart()
+}
+
+function renderCart() {
+    const cartTable = document.getElementById('cartTable')
+    cartTable.innerHTML = ""
+    
+    Object.keys(cart).forEach(key => showCartItem(cart[key][1], cart[key][0]))
+}
+
 
 function downLoadAllGames() {
 
@@ -101,4 +172,77 @@ function downLoadAllGames() {
     .then(data => {
        showAllGames(data)
     })
+}
+
+function searchBar() {
+    const myInput = getElementById('myInput')
+    searchBar.addEventListener('keyup', e => e.target.value)
+}
+
+
+function showCartItem(item, counter) {
+    
+    const tr = document.createElement('tr')
+    cartTable.append(tr)
+
+    const td = document.createElement('td')
+    tr.append(td)
+    td.className = "product-name"
+
+    const divProductTn = document.createElement('div')
+    td.append(divProductTn)
+    divProductTn.className = "product-thumbnail"
+
+    const img = document.createElement('img')
+    divProductTn.append(img)
+    img.src = item.thumbnail
+    img.alt = item.title
+
+    const divproductDt = document.createElement('div')
+    td.append(divproductDt)
+    divproductDt.className = "product-detail"
+
+    const h3 = document.createElement('h3')
+    divproductDt.append(h3)
+    h3.className = "product-title"
+    h3.innerText = item.title
+
+    const p = document.createElement('p')
+    divproductDt.append(p)
+    p.innerText = item.short_description
+
+    const tdProductPr = document.createElement('td')
+    tr.append(tdProductPr)
+    tdProductPr.className = "product-price"
+    tdProductPr.innerText = "$15.99"
+
+    const tdProductQty = document.createElement('td')
+    tr.append(tdProductQty)
+    tdProductPr.className = "product-qty"
+
+    const select = document.createElement('select')
+    tdProductQty.append(select)
+    select.name = "#"
+
+    const option1 = document.createElement('option')
+    select.append(option1)
+    option1.value = "1"
+    option1.innerText = counter
+
+    const tdProductTl = document.createElement('td')
+    tr.append(tdProductTl)
+    tdProductTl.className = "product-total"
+    tdProductTl.innerText = "$15.99"
+
+    const tdAction = document.createElement('td')
+    tr.append(tdAction)
+    tdAction.className = "action"
+    const tdActionAnchor = document.createElement('a')
+    tdAction.append(tdActionAnchor)
+    // tdActionAnchor.href = "#"
+    const i = document.createElement('i')
+    tdActionAnchor.append(i)
+    i.className = "fa fa-times"
+
+    tdActionAnchor.addEventListener('click', () => deleteFromCart(item))
 }
